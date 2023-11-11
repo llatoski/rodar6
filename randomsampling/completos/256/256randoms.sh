db3d1721541aa4ed7f05505cd0aef283e3987941
Luis Carlos Latoski <latoski@lovelace2.if.ufrgs.br>
2023-11-11 20:11:44 -0300

    Rodando o final...

--- randomsampling/completos/256/256randoms.sh
+++ randomsampling/completos/256/256randoms.sh
@@ -9,45 +9,6 @@
 SAMP=100
 SIZ=256
 
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.05963623316594643 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.07543120063354623 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.09540954763499944 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.1206792640639329 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.15264179671752334 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.19306977288832497 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.244205309454865 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.30888435964774846 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.3906939937054621 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
-
-gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.49417133613238384 -o inc.out -std=c99
-./inc.out
-rm -rf inc.out
 
 gcc randomsamples.c -lm -O3 -D L=$SIZ -D SAMPLES=$SAMP -D DETA=0.6250551925273976 -o inc.out -std=c99
 ./inc.out
